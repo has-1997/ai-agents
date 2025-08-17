183ce630e41512bed1813eeacf1b73046a5bf514
has-1997 <scammaps@protonmail.com>
2025-08-17 16:31:55 +0100

    Chain of thought - Structured Outputs for chain-of-thought math tutoring

--- openai-api/structured_output.py
+++ openai-api/structured_output.py
@@ -19,7 +19,7 @@ model = "gpt-5-nano-2025-08-07"
 #         {
 #             "role": "system",
 #             "content": "Extract the event information."
-#         }, 
+#         },
 #         {
 #             "role": "user",
 #             "content": "Alice and Bob are going to a science fair on Friday."
@@ -31,3 +31,30 @@ model = "gpt-5-nano-2025-08-07"
 # event = response.output_parsed
 
 # print(event)
+
+
+# Examples
+# Chain of thought - Structured Outputs for chain-of-thought math tutoring
+# class Step(BaseModel):
+#     explanation: str
+#     output: str
+
+# class MathReasoning(BaseModel):
+#     steps: list[Step]
+#     final_answer: str
+
+# response = client.responses.parse(
+#     model=model,
+#     input=[
+#         {
+#             "role": "system",
+#             "content": "You are a helpful math tutor. Guide the user through the solution step by step.",
+#         },
+#         {"role": "user", "content": "how can I solve 8x + 7 = -23"},
+#     ],
+#     text_format=MathReasoning,
+# )
+
+# math_reasoning = response.output_parsed
+
+# print(math_reasoning)
